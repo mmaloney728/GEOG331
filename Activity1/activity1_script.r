8f78fb005dfa4125ec9eae93d5a6434ef4788675
Megan Maloney '20 <mmaloney@colgate.edu>
2020-01-24 12:34:16 -0500

    added an r script for activity 1

--- Activity1/activity1_script.r
+++ Activity1/activity1_script.r
@@ -3,14 +3,25 @@ print("hello R world")
 
 # three times three
 3*3
+# twenty-seven minus eight
+27-8
+# five divided by three
+5/3
 
 # saved variable calculations
 a <- 450006948
+b <- 57
+s <- 462
 
 a*1000
 a/10
 
+b*s
+a/s
+b+a
+
 #vector math - don't need for loops in R
 v <- c(3,4,5,6)
-
+t <- c(9,2,2,6,7)
 v*a
+t+s
