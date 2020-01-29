bfe719bd4394d00bb338e1fa5263bbdbb8ff1e82
Megan Maloney '20 <mmaloney@colgate.edu>
2020-01-29 11:41:50 -0500

    started a script for activity 2

---
+++ Activity2/activity2_script.R
@@ -0,0 +1,61 @@
+#make a vector of tree heights in meters
+heights <- c(30,41,20,22)
+#convert to cm
+heights_cm <- heights*100
+heights_cm
+
+#look at the first tree height
+heights[1]
+#look at the 2nd and 3rd tree heights
+heights[2:3]
+
+#get more info on the matrix function
+help(matrix)
+
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+Mat
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+Mat.bycol
+#subset the matrix to look at row 1, column2
+Mat.bycol[1,2]
+#look at all values in row 1
+Mat.bycol[1,]
+#look at all values in column 2
+Mat.bycol[,2]
+
+
+#read in weather station file from the data folder
+datW <- read.csv("y:\\Students\\hkropp\\a02\\2011124.csv")
+
+#get more information about the dataframe
+str(datW)
+
+#changing date format from a factor
+#specify a column with a proper date format
+#note the format here dataframe$column
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+#google date formatting in r to find more options and learn more
+
+#creating a numeric year column
+#create a date column by reformatting the date to only include years
+#and indicating that it should be treated as numeric data
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+
+#Question 2: Example Vectors of each data type.
+#character:
+char.vec <- c("Example", "character", "vector", "1")
+
+
+
+
+
+
+
+
+
+
+
