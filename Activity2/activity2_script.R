ef47ae04173a8abdaa0afdbbedda6ccfc8464211
Megan Maloney '20 <mmaloney@colgate.edu>
2020-01-31 13:45:14 -0500

    started a script for activity 2

--- Activity2/activity2_script.R
+++ Activity2/activity2_script.R
@@ -1,3 +1,4 @@
+#VECTORS
 #make a vector of tree heights in meters
 heights <- c(30,41,20,22)
 #convert to cm
@@ -9,6 +10,7 @@ heights[1]
 #look at the 2nd and 3rd tree heights
 heights[2:3]
 
+#MATRICES
 #get more info on the matrix function
 help(matrix)
 
@@ -27,7 +29,7 @@ Mat.bycol[1,]
 #look at all values in column 2
 Mat.bycol[,2]
 
-
+#DATA FRAMES
 #read in weather station file from the data folder
 datW <- read.csv("y:\\Students\\hkropp\\a02\\2011124.csv")
 
@@ -45,7 +47,7 @@ datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
 #and indicating that it should be treated as numeric data
 datW$year <- as.numeric(format(datW$dateF,"%Y"))
 
-#Question 2: Example Vectors of each data type.
+#QUESTION 2: Example Vectors of each data type.
 #character:
 char.vec <- c("Example", "character", "vector", "1", "27.4")
 numeric.vec <- c(2.1, 4, 6.3, -8, 10)
@@ -54,7 +56,7 @@ integer.vec <- c(2L, 4L, 0L, -2L, 15L)
 
 
 
-
+#DESCRIPTIVE STATISTICS AND HISTOGRAMS
 #find out all unique site names
 levels(datW$NAME)
 
@@ -124,7 +126,7 @@ abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE) + sd(datW$TAVE[datW$siteN
        lty = 3,
        lwd = 3)
 
-#Question 4
+#QUESTION 4
 par(mfrow=c(2,2))
 #histogram for site 1 -- Aberdeen, WA
 hist(datW$TAVE[datW$siteN == 1],
@@ -151,7 +153,7 @@ hist(datW$TAVE[datW$siteN==2],
      main = paste(levels(datW$NAME)[2]),
      xlab = "Average daily temperature (degrees C)", 
      ylab="Relative frequency",
-     col="navy blue",
+     col="goldenrod",
      border="white")
 abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE), 
        col = "tomato3",
@@ -203,3 +205,152 @@ abline(v = mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE) + sd(datW$TAVE[datW$siteN
        lty = 3,
        lwd = 3)
 
+#PROBABILITY DISTRIBUTIONS
+#make a histogram for the first site in our levels
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+#note I've named the histogram so I can reference it later
+h1 <- hist(datW$TAVE[datW$siteN == 1],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[1]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="grey50",
+           border="white")
+#the seq function generates a sequence of numbers that we can use to plot the normal across the range of temperature values
+x.plot <- seq(-10,30, length.out = 100)
+#the dnorm function will produce the probability density based on a mean and standard deviation.
+
+y.plot <-  dnorm(seq(-10,30, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#create a density that is scaled to fit in the plot  since the density has a different range from the data density.
+#!!! this is helpful for putting multiple things on the same plot
+#!!! It might seem confusing at first. It means the maximum value of the plot is always the same between the two datasets on the plot. Here both plots share zero as a minimum.
+y.scaled <- (max(h1$density)/max(y.plot)) * y.plot
+
+#points function adds points or lines to a graph  
+#the first two arguements are the x coordinates and the y coordinates.
+
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+#QUESTION 5
+par(mfrow=c(2,2))
+h1 <- hist(datW$TAVE[datW$siteN == 1],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[1]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="grey50",
+           border="white")
+x.plot <- seq(-10,30, length.out = 100)
+y.plot <-  dnorm(seq(-10,30, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+y.scaled <- (max(h1$density)/max(y.plot)) * y.plot
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+h2 <- hist(datW$TAVE[datW$siteN == 2],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[2]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="goldenrod",
+           border="white")
+x.plot <- seq(0,30, length.out = 100)
+y.plot <- dnorm(seq(0,30, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 2],na.rm=TRUE)).
+y.scaled <- (max(h2$density)/max(y.plot)) * y.plot
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+h3 <- hist(datW$TAVE[datW$siteN == 3],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[3]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="light green",
+           border="white")
+x.plot <- seq(-40,40, length.out = 100)
+y.plot <-  dnorm(seq(-40,40, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 3],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 3],na.rm=TRUE)) 
+y.scaled <- (max(h3$density)/max(y.plot)) * y.plot
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+h4 <- hist(datW$TAVE[datW$siteN == 4],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[4]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="light blue",
+           border="white")
+x.plot <- seq(0,40, length.out = 100)
+y.plot <-  dnorm(seq(0,40, length.out = 100),
+                 mean(datW$TAVE[datW$siteN == 4],na.rm=TRUE),
+                 sd(datW$TAVE[datW$siteN == 4],na.rm=TRUE)) 
+y.scaled <- (max(h4$density)/max(y.plot)) * y.plot
+points(x.plot,
+       y.scaled, 
+       type = "l", 
+       col = "royalblue3",
+       lwd = 4, 
+       lty = 2)
+
+
+#PROBABILITY DISTRIBUTIONS
+help(dnorm)
+#pnorm(value to evaluate at (note this will evaluate for all values and below),mean, standard deviation)
+pnorm(0,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#pnrom with 5 gives me all probability (area of the curve) below 5 
+pnorm(5,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#pnrom with 5 gives me all probability (area of the curve) below 5 
+pnorm(5,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))- pnorm(0,
+                                                        mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+                                                        sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#pnrom of 20 gives me all probability (area of the curve) below 20 
+#subtracting from one leaves me with the area above 20
+1 - pnorm(20,
+          mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+          sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#using qnorm to find value at which temperatures above 95% of data start
+qnorm(0.95,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#QUESTION 6
+#extreme high temperatures occur less than 10% of the time
+#This means the current threshold is the 90th percentile of data
+threshold <- qnorm(0.90,
+      mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE),
+      sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+#now we want the probability that data with a mean increased by 4 is greater than this current threshold
+1 - pnorm(threshold,
+          mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE)+4,
+          sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE))
+
+#QUESTION 7
