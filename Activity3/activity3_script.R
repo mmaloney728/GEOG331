6412e579c6dfdba7516c991eac70d4821c6eec5b
Megan Maloney '20 <mmaloney@colgate.edu>
2020-02-19 13:04:33 -0500

    updated r script for activity 3

--- Activity3/activity3_script.R
+++ Activity3/activity3_script.R
@@ -40,6 +40,9 @@ colnames(datW) <-   colnames(sensorInfo)
 #preview data
 print(datW[1,])
 
+#QUESTION 3 
+#What is the difference btwn skip and nrows in these two read.csv commands?
+#what does header=FALSE do?
 
 #use install.packages to install lubridate
 #install.packages(c("lubridate"))
@@ -50,3 +53,109 @@ library(lubridate)
 #convert to standardized format
 #date format is m/d/y
 dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calcualtions
+datW[1,]
+
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+#wind speed
+length(which(is.na(datW$wind.speed)))
+#precipitation
+length(which(is.na(datW$precipitation)))
+#soil temperature
+length(which(is.na(datW$soil.moisture)))
+#soil moisture
+length(which(is.na(datW$soil.temp)))
+#a lot of missing values from soil sensor
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+#I'm going to make a new column to work with that indicates that I am conducting QAQC
+#because overwriting values should be done cautiously and can lead to confusing issues.
+#It can be particularily confusing when you are just learning R.
+#Here I'm using the ifelse function
+#the first argument is a logical statement to be evaluated as true or false on a vector
+#the second argument is the value that my air.tempQ1 column will be given if the statement
+#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+#In this case it is just given the air temperature value
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,] 
+
+#QUESTION 4
+#Are the extreme high and low values in this dataset reliably measured by the sensor? Explain your answer.
+
+
+#plot precipitation and lightning strikes on the same plot
+#normalize lighting strikes to match precipitation
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+#make the plot with precipitation and lightning activity marked
+#make it empty to start and add in features
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+#plot precipitation points only when there is precipitation 
+#make the points semi-transparent
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
+
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
+
+#QUESTION 5
+
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+#QUESTION 6
+#removing suspect measurements
+
+#test using assert to verify that his filtered the data as expected
+
+#describe outcome
+
+#plot with both lines and points of windspeed with the new data
+
+#QUESTION 7
+#check that soil temp & moisture measurements are reliable in days leading up to soil sensor outage
+#explain reasoning, show code
+
+#QUESTION 8
+#table with avg air temp, wind speed, soil moisture, soil temp, total precip
+#how many observations 
+#time period of measurement
+#correct number decimal places that is within sensor error.
+
+#QUESTION 9
+#plot soil moisture
+#plot air temp
+#plot soil temp
+#plot precip
+#all with same x range
+#describe trends in a few sentences
--- Activity4/activity4_script.R
+++ Activity4/activity4_script.R
@@ -1,5 +1,7 @@
+#Megan Maloney -- Activity 4
 #use built in iris dataset
 #take a look at it 
+#install.packages(c("ggplot2","dplyr"))
 head(iris)
 #load in some tidyverse packages
 library(dplyr)
@@ -16,7 +18,25 @@ library(ggplot2)
 #1. iris  sepal length x width
 #2. iris  petal length x width
 #3. iris sepal length x petal length
+versicolor <- iris[iris$Species == "versicolor", ]
+versicolor$Sepal.Length
+#y ~ x
+#dependent variable "is a function of" independent variable
+#lm.out <- lm(versicolor[,"Sepal.Width"] ~ versicolor[,"Sepal.Length"])
+#str(lm.out) # is a list
 
+x <- c("Sepal.Length", "Petal.Length", "Sepal.Length")
+y <- c("Sepal.Width", "Petal.Width", "Petal.Length")
+
+lm.out <- list()
+#index lists with double brackets
+#if data is classified as factor, may need to use paste() ex'l: versicolor[ ,paste(y[i])]
+for(i in 1:3){
+  lm.out[[i]] <- lm(versicolor[ ,y[i]] ~ versicolor[ ,x[i]])
+}
+
+lm.out[1]
+lm.out[2]
 
 
 #####################################
@@ -25,10 +45,13 @@ library(ggplot2)
 
 #use dplyr to join data of maximum height
 #to a new iris data frame
+
 height <- data.frame(Species = c("virginica","setosa","versicolor"),
                      Height.cm = c(60,100,11.8))
 
-
+#iris left
+#height right
+iris2 <- left_join(iris, height, by="Species")
 
 #####################################
 ##### Part 3: plots in ggplot2  #####
@@ -37,16 +60,23 @@ height <- data.frame(Species = c("virginica","setosa","versicolor"),
 plot(iris$Sepal.Length,iris$Sepal.Width)
 
 #3a. now make the same plot in ggplot
-
+ggplot(data = iris, aes(Sepal.Length, Sepal.Width)) +
+  geom_point()
 
 #3b. make a scatter plot with ggplot and get rid of  busy grid lines
-
+ggplot(data = iris, aes(Sepal.Length, Sepal.Width)) +
+  geom_point()+
+  theme_classic()
 
 #3c.make a scatter plot with ggplot and get rid of grid lines
 #and show species by color increasing the point size
+ggplot(data = iris, aes(Sepal.Length, Sepal.Width, color=Species)) +
+  geom_point(size=4)+
+  theme_classic()
 
 #####################################
 ##### Question: how did         #####
 ##### arguments differ between  #####
 ##### plot and ggplot?          #####
 #####################################		
+#There is a syntax change as we move to ggplot in names such as color and size.
