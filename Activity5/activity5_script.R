424a42d1724ac557f87aa75c62ffee2b261026d7
Megan Maloney '20 <mmaloney@colgate.edu>
2020-02-24 12:13:15 -0500

    added an r script for activity 5

---
+++ Activity5/activity5_script.R
@@ -0,0 +1,138 @@
+#load in lubridate
+library(lubridate)
+#read in streamflow data
+datH <- read.csv("y:\\Data\\activities\\a05\\stream_flow_data.csv",
+                 na.strings = c("Eqp"))
+head(datH)     
+#read in precipitation data
+#hourly precipitation is in mm
+datP <- read.csv("y:\\Data\\activities\\a05\\2049867.csv")                          
+head(datP)
+#only use most reliable measurements
+datD <- datH[datH$discharge.flag == "A",]
+
+#### define time for streamflow #####
+#convert date and time
+datesD <- as.Date(datD$date, "%m/%d/%Y")
+#get day of year
+datD$doy <- yday(datesD)
+#calculate year
+datD$year <- year(datesD)
+#define time
+timesD <- hm(datD$time)
+
+#### define time for precipitation #####    
+dateP <- ymd_hm(datP$DATE)
+#get day of year
+datP$doy <- yday(dateP)
+#get year 
+datP$year <- year(dateP)
+
+#### get decimal formats #####
+#convert time from a string to a more usable format
+#with a decimal hour
+datD$hour <- hour(timesD ) + (minute(timesD )/60)
+#get full decimal time
+datD$decDay <- datD$doy + (datD$hour/24)
+#calculate a decimal year, but account for leap year
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/367),
+                       datD$year + (datD$decDay/366))
+#calculate times for datP                       
+datP$hour <- hour(dateP ) + (minute(dateP )/60)
+#get full decimal time
+datP$decDay <- datP$doy + (datP$hour/24)
+#calculate a decimal year, but account for leap year
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/367),
+                       datP$year + (datP$decDay/366))        
+
+#plot discharge
+plot(datD$decYear, datD$discharge, type="l", xlab="Year", ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+
+#QUESTION 3
+#number of observations
+# streamflow:
+length(datD$time)
+# there are 393798 streamflow observations.
+# precipitation:
+length(datP$doy)
+# there are 16150 precipitation observations. 
+
+#basic formatting
+aveF <- aggregate(datD$discharge, by=list(datD$doy), FUN="mean")
+colnames(aveF) <- c("doy","dailyAve")
+sdF <- aggregate(datD$discharge, by=list(datD$doy), FUN="sd")
+colnames(sdF) <- c("doy","dailySD")
+#start new plot
+dev.new(width=8,height=8)
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2)
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i")#remove gaps from axes  
+#show standard deviation around the mean
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)       
+axis(1, seq(0,360, by=40), #tick intervals
+     lab=seq(0,360, by=40)) #tick labels
+axis(2, seq(0,80, by=20),
+     seq(0,80, by=20),
+     las = 2)#show ticks at 90 degree angle
+
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)       
+axis(1, seq(0,360, by=40), #tick intervals
+     lab=seq(0,360, by=40)) #tick labels
+axis(2, seq(0,80, by=20),
+     seq(0,80, by=20),
+     las = 2)#show ticks at 90 degree angle
+legend("topright", c("mean","1 standard deviation"), #legend items
+       lwd=c(2,NA),#lines
+       fill=c(NA,rgb(0.392, 0.584, 0.929,.2)),#fill boxes
+       border=NA,#no border for both fill boxes (don't need a vector here since both are the same)
+       bty="n")#no legend border
