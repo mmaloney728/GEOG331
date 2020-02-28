3f53f103707c47a66723423fc26a9d0bfe144cd6
Megan Maloney '20 <mmaloney@colgate.edu>
2020-02-28 15:13:45 -0500

    updated r script for activity 5

--- Activity5/activity5_script.R
+++ Activity5/activity5_script.R
@@ -35,15 +35,15 @@ datD$hour <- hour(timesD ) + (minute(timesD )/60)
 #get full decimal time
 datD$decDay <- datD$doy + (datD$hour/24)
 #calculate a decimal year, but account for leap year
-datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/367),
-                       datD$year + (datD$decDay/366))
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + ((datD$decDay-1)/366),
+                       datD$year + ((datD$decDay-1)/365))
 #calculate times for datP                       
 datP$hour <- hour(dateP ) + (minute(dateP )/60)
 #get full decimal time
 datP$decDay <- datP$doy + (datP$hour/24)
 #calculate a decimal year, but account for leap year
-datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/367),
-                       datP$year + (datP$decDay/366))        
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + ((datP$decDay-1)/366),
+                       datP$year + ((datP$decDay-1)/365))        
 
 #plot discharge
 plot(datD$decYear, datD$discharge, type="l", xlab="Year", ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
@@ -136,3 +136,172 @@ legend("topright", c("mean","1 standard deviation"), #legend items
        fill=c(NA,rgb(0.392, 0.584, 0.929,.2)),#fill boxes
        border=NA,#no border for both fill boxes (don't need a vector here since both are the same)
        bty="n")#no legend border
+
+#IMPROVED LEGEND
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)       
+axis(1, seq(0,360, by=40), #tick intervals
+     lab=seq(0,360, by=40)) #tick labels
+axis(2, seq(0,80, by=20),
+     seq(0,80, by=20),
+     las = 2)#show ticks at 90 degree angle
+legend("topright", c("mean","1 standard deviation"), #legend items
+       lwd=c(2,NA),#lines
+       col=c("black",rgb(0.392, 0.584, 0.929,.2)),#colors
+       pch=c(NA,15),#symbols
+       bty="n")#no legend border
+
+#QUESTION 5
+#add a line that shows the observations for 2017 onto this graph of the average
+# you may have to adjust the axes limits 
+# make the 2017 line a different color than the current colors
+
+#bigger margins
+par(mai=c(1,1,1,1))
+#make plot
+plot(aveF$doy,aveF$dailyAve, 
+     type="l", 
+     xlab="Year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2,
+     ylim=c(0,90),
+     xaxs="i", yaxs ="i",#remove gaps from axes
+     axes=FALSE)#no axes
+polygon(c(aveF$doy, rev(aveF$doy)),#x coordinates
+        c(aveF$dailyAve-sdF$dailySD,rev(aveF$dailyAve+sdF$dailySD)),#ycoord
+        col=rgb(0.392, 0.584, 0.929,.2), #color that is semi-transparent
+        border=NA#no border
+)       
+lines(datD$decDay[which(datD$year==2017)], datD$discharge[which(datD$year==2017)],
+      type = "l",
+      col="blue")
+# change the x axis label so that they show each month instead of doy
+monthvec <- c(1,32,60,91,121,152,182,213,244,274,305,335,365)
+name <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"," ")
+axis(1, monthvec, #tick intervals
+     lab=name) #tick labels
+axis(2, seq(0,80, by=20),
+     seq(0,80, by=20),
+     las = 2)#show ticks at 90 degree angle
+legend("topright", c("mean","1 standard deviation","2017 Observations"), #legend items
+       lwd=c(3,NA),#lines
+       col=c("black",rgb(0.392, 0.584, 0.929,.2), "blue"),#colors
+       pch=c(NA,15, NA),#symbols
+       bty="n")#no legend border
+
+
+
+#QUESTION 7
+#create a dataframe that indicates what days have a full 24 hours of precip measurements
+#grouping by number of observations each day
+obsframe <- aggregate(datP, by=list(datP$doy, datP$year), FUN = length)
+#pulling out value with all 24 measurements
+full.meas <- data.frame(doy=obsframe$Group.1[which(obsframe$doy==24)], 
+                        year=obsframe$Group.2[which(obsframe$doy==24)])
+ 
+full.meas$decYear <- ifelse(leap_year(full.meas$year), full.meas$year + ((full.meas$doy-1)/366),
+                            full.meas$year + ((full.meas$doy-1)/365))
+#plot
+par(mai=c(1,1,1,1))
+plot(datD$decYear, datD$discharge,
+     type = "l",
+     xlab = "Date",
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")),
+     lwd=2)
+points(full.meas$decYear, rep(0, length(full.meas$doy)), pch=19,
+       col="blue")
+legend("topright", c("Discharge Measurement", "Dates"),
+       lwd = c(3, NA),
+       pch=c(NA, 19),
+       col=c("black", "blue"),
+       bty="n")
+
+
+
+
+#subsest discharge and precipitation within range of interest
+hydroD <- datD[datD$doy >= 248 & datD$doy < 250 & datD$year == 2011,]
+hydroP <- datP[datP$doy >= 248 & datP$doy < 250 & datP$year == 2011,]
+min(hydroD$discharge)
+
+#get minimum and maximum range of discharge to plot
+#go outside of the range so that it's easy to see high/low values
+#floor rounds down the integer
+yl <- floor(min(hydroD$discharge))-1
+#celing rounds up to the integer
+yh <- ceiling(max(hydroD$discharge))+1
+#minimum and maximum range of precipitation to plot
+pl <- 0
+pm <-  ceiling(max(hydroP$HPCP))+.5
+#scale precipitation to fit on the 
+hydroP$pscale <- (((yh-yl)/(pm-pl)) * hydroP$HPCP) + yl
+
+par(mai=c(1,1,1,1))
+#make plot of discharge
+plot(hydroD$decDay,
+     hydroD$discharge, 
+     type="l", 
+     ylim=c(yl,yh), 
+     lwd=2,
+     xlab="Day of year", 
+     ylab=expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#add bars to indicate precipitation 
+for(i in 1:nrow(hydroP)){
+        polygon(c(hydroP$decDay[i]-0.017,hydroP$decDay[i]-0.017,
+                  hydroP$decDay[i]+0.017,hydroP$decDay[i]+0.017),
+                c(yl,hydroP$pscale[i],hydroP$pscale[i],yl),
+                col=rgb(0.392, 0.584, 0.929,.2), border=NA)
+}
+
+#QUESTION 8
+#Choose another day to make a 2nd hydrograph during the winter
+#explain how you chose a time period
+#how do the hydrographs compare?
+#are there any limitations in interpreting hourly
+#why spikes in streamflow with no rain?
+
+
+
+
+
+library(ggplot2)
+#specify year as a factor
+datD$yearPlot <- as.factor(datD$year)
+#make a boxplot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+        geom_boxplot()
+#make a violin plot
+ggplot(data= datD, aes(yearPlot,discharge)) + 
+        geom_violin()
+
+#QUESTION 9
+#make a violin plot by season for 2016 and 2017 separately
+#2016 plot
+ggplot(data=datD[which(datD$yearPlot=="2016"),], 
+       aes(yearPlot, discharge))+
+        geom_violin()+
+        xlab("Year")+
+        ylab(expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+#2017 plot
+ggplot(data=datD[which(datD$yearPlot=="2017"),], 
+       aes(yearPlot, discharge))+
+        geom_violin()+
+        xlab("Year")+
+        ylab(expression(paste("Discharge ft"^"3 ","sec"^"-1")))
+
+
+
