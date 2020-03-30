8618da5fb4589542fb3cf1b94a75734f0f5b1b10
Megan Maloney <meganmaloney@Megans-MacBook-Pro-2.local>
2020-03-30 15:43:26 -0400

    updated R script for activity 6

--- Activity6/activity6_nppscript.R
+++ Activity6/activity6_nppscript.R
@@ -58,7 +58,7 @@ par(mai=c(1,1,1,1))
 plotRGB(rgbL, stretch="lin", axes=TRUE)
 #add polygons to plot
 #redL@crs showed us that we can overlap spatial data
-plot(g1966, col="tan3", border=NA, add=TRUE)
+plot(g1966, col="tan3", border=NA, add=TRUE) 
 plot(g1998, col="royalblue3", add=TRUE, border=NA)
 plot(g2005, col="darkgoldenrod4", add=TRUE, border=NA)
 plot(g2015, col="tomato3", add=TRUE, border=NA)
@@ -76,7 +76,8 @@ ndviYear <- seq(2003,2016)
 #read all files into a list
 NDVIraster <- list() 
 for(i in 1:length(ndviYear)){
-  NDVIraster[[i]] <- raster(paste0("~/Downloads/mmaloney/a06/NDVI/NDVI_",ndviYear[i],".tif"))
+    NDVIraster[[i]] <- raster(paste0("~/Downloads/mmaloney/a06/NDVI/NDVI_",ndviYear[i],".tif"))
+
   
 }
 #look at first year
@@ -90,13 +91,12 @@ plot(NDVIraster[[1]])
 
 #QUESTION 3
 #plot 1966 NDVI glacier polygons
+
 #2003 NDVI data side by side
-#why cant you put both data files on same map?
-# axes are different (x has positives for 1966, negatives for 2003)
-# very different ranges even though in same geographic space. putting them together they would be very small dots 
 par(mfrow=c(1,2))
 plot(NDVIraster[[1]],axes=TRUE)
 plot(g1966, axes=TRUE)
+
 
 #Glacier Retreat
 #reproject the glaciers
@@ -147,5 +147,135 @@ for(i in 2:39){
 }
 
 #QUESTION 5
+#calculate the % change in area between 1966 and 2015
+g2015p@data$pchange.area<-gAll$a2015m.sq/gAll$a1966m.sq
+#make an spplot of the glaciers in 2015 showing the % change each glacier has experienced
+spplot(g2015p,"pchange.area")
+
+
+diffPoly <- gDifference(g1966p, g2015p)
+plot(diffPoly)
+#plot with NDVI
+plot(NDVIraster[[13]], axes=FALSE, box=FALSE)
+plot(diffPoly,col="black",border=NA,add=TRUE)
+
+#QUESTION 6
+#find the glacier with the largest % loss
+largestploss<-min(g2015p@data$pchange.area)
+g2015p@data$GLACNAME[which(g2015p@data$pchange.area==largestploss)]
+1-largestploss
+# boulder glacier has the largest % loss, at only 15.28% of its area in 1966, or at 84.72% of its original area
+
+#make a map that best displays the glacial extent for all years for that glacier with the highest % loss
+#include background imagery
+#you can use the original data for the map to match with the imagery
+#add a map title that includes the % loss and glacier name
+#the subset function will be helpful here
+# coordinates for where glacier is:
+gAll[which(gAll$GLACNAME=="Boulder Glacier"),c("X_COORD","Y_COORD")]
+par(mai=c(1,1,1,1))
+par(col.axis="white", col.lab="white", tck=0)
+#zoom in on glacier
+plotRGB(rgbL, ext=c(270500,277400,5423830,5430730), stretch="lin", axes=TRUE, main="Boulder Glacier: 84.72% loss")
+box(col="white")
+plot(g1966, col="palegreen2", border=NA, add=TRUE)
+plot(g1998, col="royalblue3", add=TRUE, border=NA)
+plot(g2005, col="darkgoldenrod4", add=TRUE, border=NA)
+plot(g2015, col="tomato3", add=TRUE, border=NA)
+
+
+
+#extract NDVI values
+NDVIdiff <- list()
+meanDiff <- numeric(0)
+#loop through all NDVI years
+for(i in 1:length(ndviYear)){
+	#get raster values in the difference polygon
+	NDVIdiff[[i]] <- extract(NDVIraster[[i]],diffPoly)[[1]]
+	#calculate the mean of the NDVI values
+	meanDiff[i] <- mean(NDVIdiff[[i]], na.rm=TRUE)
+}
+plot(ndviYear, meanDiff, type="b",
+xlab="Year",
+ylab="Average NDVI (unitless)",
+pch=19)
+
+#designate that NDVIraster list is a stack
+NDVIstack <- stack(NDVIraster)
+#set up lm function to apply to every cell
+#where x is the value of a cell
+#need to first skip NA values (like lakes)
+#if NA is missing in first raster, it is missing in all
+#so we can tell R to assign an NA rather than fitting the function
+timeT <- ndviYear
+fun <- function(x) {
+	if(is.na(x[1])){
+		NA}else{
+			#fit a regression and extract a slope
+			lm(x ~ timeT)$coefficients[2] }}
+#apply the slope function to the rasters		
+NDVIfit <- calc(NDVIstack, fun)
+#plot the change in NDVI
+plot(NDVIfit, axes=FALSE)
+
+
+#buffer glaciers
+glacier500m <- gBuffer(g1966p, #data to buffer 
+byid=TRUE, #keeps original shape id
+width=500) #width in coordinate system units
+
+#convert to a raster
+buffRaster <- rasterize(glacier500m,#vector to convert to raster
+NDVIraster[[1]], #raster to match cells and extent
+field=glacier500m@data$GLACNAME, #field to convert to raster data
+background=0) #background value for missing data
+plot(buffRaster)
+
+#rasterize glaciers
+glacRaster <- rasterize(g1966p, NDVIraster[[1]],
+field=g1966p@data$GLACNAME, background=0)
+#subtract buffer from original glacier
+glacZones <- buffRaster - glacRaster
+plot(glacZones)
+
+
+meanChange <- zonal(NDVIfit, #NDVI function to summarize
+glacZones, #raster with zones
+"mean")#function to apply
+head(meanChange)
+
+#QUESTION 9
+#Add the mean change in NDVI per year into the 2015 glacier polygons.
+#make a map where the mean change in vegetation is color coded within the 2015 glacier polygons
+#remove zone zero and include column with data, not zone labels
+#add to 2015 data
+g2015p@data$meanChange<-meanChange[-1,2]
+spplot(g2015p,"meanChange",col="transparent")
+
+
+
+
+#QUESTION 11
+# mean NDVI through all years per pixel
+NDVImeanpp<-calc(NDVIstack,mean, na.rm=TRUE) 
+spplot(NDVImeanpp)
+# mean of each zone (glacier)
+totalMean<-zonal(NDVImeanpp,glacZones,"mean")
+# remove zone zero
+g2015p@data$NDVImean<-totalMean[-1,2]
+
+colorR <- brewer.pal(4, "PuOr")
+colorR <- c("#3333FF", "#00CCFF", "#FF3300", "#000033")
+g2015p@data$NDVIcol <- ifelse(g2015p@data$NDVImean<0.2, colorR[1],
+	ifelse(g2015p@data$NDVImean>=0.2 & g2015p@data$NDVImean<0.4, colorR[2],
+	ifelse(g2015p@data$NDVImean>=0.4 & g2015p@data$NDVImean<0.6, colorR[3],
+	colorR[4])))
+
+#plot average maximum for all years
+plot(NDVImeanpp)
+#overlay color-coded means around buffered glaciers
+plot(g2015p, add=TRUE, col=paste(g2015p@data$NDVIcol), border=FALSE)
+legend("topright", c("0-0.2", "0.2-0.4", "0.4-0.6", "0.6-1"), fill=colorR, 
+	title="Glacier NDVI Means", cex=0.75)
 
 
